b44c97341b97c4ee7b255b8aa352a9b457e7ccf6
Xen <lordofxen@deskasoft.com>
2025-08-31 01:21:06 +0300

    Push new unit tests for the latest xxHash implementation
    Signed-off-by: Xen <lordofxen@deskasoft.com>

---
+++ HashifyNet.UnitTests/Algorithms/XxHash/XxHash3_Implementation_Tests.cs
@@ -0,0 +1,182 @@
+// *
+// *****************************************************************************
+// *
+// * Copyright (c) 2025 Deskasoft International
+// *
+// * Permission is hereby granted, free of charge, to any person obtaining a copy
+// * of this software and associated documentation files (the "Software"), to deal
+// * in the Software without restriction, including without limitation the rights
+// * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// * copies of the Software, and to permit persons to whom the Software is
+// * furnished to do so, subject to the following conditions:
+// *
+// * The above copyright notice and this permission notice shall be included in all
+// * copies or substantial portions of the Software.
+// *
+// * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// * SOFTWARE.
+// *
+// *
+// * Please refer to LICENSE file.
+// *
+// ******************************************************************************
+// *
+
+using HashifyNet.Algorithms.XxHash3;
+using HashifyNet.UnitTests.Utilities;
+using Moq;
+
+namespace HashifyNet.UnitTests.Algorithms.XxHash3
+{
+	public class XxHash3_Implementation_Tests
+	{
+		#region Constructor
+
+		[Fact]
+		public void XxHash3_Implementation_Constructor_ValidInputs_Works()
+		{
+			var xxHashConfigMock = new Mock<IXxHash3Config>();
+			{
+				xxHashConfigMock.SetupGet(xhc => xhc.HashSizeInBits)
+					.Returns(64);
+
+				xxHashConfigMock.SetupGet(xhc => xhc.Seed)
+					.Returns(0UL);
+
+				xxHashConfigMock.Setup(xhc => xhc.Clone())
+					.Returns(() => xxHashConfigMock.Object);
+			}
+
+			GC.KeepAlive(
+				new XxHash3_Implementation(xxHashConfigMock.Object));
+		}
+
+		#region Config
+
+		[Fact]
+		public void XxHash3_Implementation_Constructor_Config_IsNull_Throws()
+		{
+			Assert.Equal(
+				"config",
+				Assert.Throws<ArgumentNullException>(
+						() => new XxHash3_Implementation(null))
+					.ParamName);
+		}
+
+		[Fact]
+		public void XxHash3_Implementation_Constructor_Config_IsCloned()
+		{
+			var xxHashConfigMock = new Mock<IXxHash3Config>();
+			{
+				xxHashConfigMock.Setup(xhc => xhc.Clone())
+					.Returns(() => new XxHash3Config()
+					{
+						HashSizeInBits = 64,
+					});
+			}
+
+			GC.KeepAlive(
+				new XxHash3_Implementation(xxHashConfigMock.Object));
+
+			xxHashConfigMock.Verify(xhc => xhc.Clone(), Times.Once);
+
+			xxHashConfigMock.VerifyGet(xhc => xhc.HashSizeInBits, Times.Never);
+			xxHashConfigMock.VerifyGet(xhc => xhc.Seed, Times.Never);
+		}
+
+		#region HashSizeInBits
+
+		[Fact]
+		public void XxHash3_Implementation_Constructor_Config_HashSizeInBits_IsInvalid_Throws()
+		{
+			var invalidHashSizes = new[] { -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 31, 33, 63, 65, 127, 129, 65535 };
+
+			foreach (var invalidHashSize in invalidHashSizes)
+			{
+				var xxHashConfigMock = new Mock<IXxHash3Config>();
+				{
+					xxHashConfigMock.SetupGet(pc => pc.HashSizeInBits)
+						.Returns(invalidHashSize);
+
+					xxHashConfigMock.Setup(pc => pc.Clone())
+						.Returns(() => xxHashConfigMock.Object);
+				}
+
+				Assert.Equal(
+					"config.HashSizeInBits",
+					Assert.Throws<ArgumentOutOfRangeException>(
+							() => new XxHash3_Implementation(xxHashConfigMock.Object))
+						.ParamName);
+			}
+		}
+
+		[Fact]
+		public void XxHash3_Implementation_Constructor_Config_HashSizeInBits_IsValid_Works()
+		{
+			var validHashSizes = new[] { 64, 128 };
+
+			foreach (var validHashSize in validHashSizes)
+			{
+				var xxHashConfigMock = new Mock<IXxHash3Config>();
+				{
+					xxHashConfigMock.SetupGet(pc => pc.HashSizeInBits)
+						.Returns(validHashSize);
+
+					xxHashConfigMock.Setup(pc => pc.Clone())
+						.Returns(() => xxHashConfigMock.Object);
+				}
+
+				GC.KeepAlive(
+					new XxHash3_Implementation(xxHashConfigMock.Object));
+			}
+		}
+
+		#endregion
+
+		#endregion
+
+		#endregion
+
+		public class IStreamableHashFunction_Tests_xxHash3_DefaultConstructor
+			: IStreamableHashFunction_TestBase<IXxHash3, IXxHash3Config>
+		{
+			protected override IEnumerable<KnownValue> KnownValues { get; } =
+				new KnownValue[] {
+					new KnownValue(64, TestConstants.Empty, "2d06800538d394c2"),
+					new KnownValue(64, TestConstants.FooBar, "d78fda63144c5c84"),
+					new KnownValue(64, TestConstants.LoremIpsum, "060828d267c7d477"),
+				};
+
+			protected override IXxHash3 CreateHashFunction(int hashSize) =>
+				new XxHash3_Implementation(
+					new XxHash3Config());
+		}
+
+		public class IStreamableHashFunction_Tests_xxHash3
+		: IStreamableHashFunction_TestBase<IXxHash3, IXxHash3Config>
+		{
+			protected override IEnumerable<KnownValue> KnownValues { get; } =
+				new KnownValue[] {
+					new KnownValue(64, TestConstants.Empty, "2d06800538d394c2"),
+					new KnownValue(64, TestConstants.FooBar, "d78fda63144c5c84"),
+					new KnownValue(64, TestConstants.LoremIpsum, "060828d267c7d477"),
+
+					new KnownValue(128, TestConstants.Empty, "99aa06d3014798d86001c324468d497f"),
+					new KnownValue(128, TestConstants.FooBar, "3c9e102628997f44ac87b0b131c6992d"),
+					new KnownValue(128, TestConstants.LoremIpsum, "6af7a84c00767a0d77d1b81bbaa39705"),
+				};
+
+			protected override IXxHash3 CreateHashFunction(int hashSize) =>
+				new XxHash3_Implementation(
+					new XxHash3Config()
+					{
+						HashSizeInBits = hashSize
+					});
+		}
+	}
+}
---
+++ HashifyNet.UnitTests/Algorithms/XxHash/XxHash_Implementation_Tests.cs
@@ -0,0 +1,283 @@
+// *
+// *****************************************************************************
+// *
+// * Copyright (c) 2025 Deskasoft International
+// *
+// * Permission is hereby granted, free of charge, to any person obtaining a copy
+// * of this software and associated documentation files (the "Software"), to deal
+// * in the Software without restriction, including without limitation the rights
+// * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// * copies of the Software, and to permit persons to whom the Software is
+// * furnished to do so, subject to the following conditions:
+// *
+// * The above copyright notice and this permission notice shall be included in all
+// * copies or substantial portions of the Software.
+// *
+// * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// * SOFTWARE.
+// *
+// *
+// * Please refer to LICENSE file.
+// *
+// ******************************************************************************
+// *
+
+using HashifyNet.Algorithms.XxHash;
+using HashifyNet.UnitTests.Utilities;
+using Moq;
+using Xunit.Abstractions;
+
+namespace HashifyNet.UnitTests.Algorithms.XxHash
+{
+	public class XxHash_Implementation_Tests
+	{
+		#region Constructor
+
+		[Fact]
+		public void XxHash_Implementation_Constructor_ValidInputs_Works()
+		{
+			var xxHashConfigMock = new Mock<IXxHashConfig>();
+			{
+				xxHashConfigMock.SetupGet(xhc => xhc.HashSizeInBits)
+					.Returns(32);
+
+				xxHashConfigMock.SetupGet(xhc => xhc.Seed)
+					.Returns(0UL);
+
+				xxHashConfigMock.Setup(xhc => xhc.Clone())
+					.Returns(() => xxHashConfigMock.Object);
+			}
+
+			GC.KeepAlive(
+				new XxHash_Implementation(xxHashConfigMock.Object));
+		}
+
+		#region Config
+
+		[Fact]
+		public void XxHash_Implementation_Constructor_Config_IsNull_Throws()
+		{
+			Assert.Equal(
+				"config",
+				Assert.Throws<ArgumentNullException>(
+						() => new XxHash_Implementation(null))
+					.ParamName);
+		}
+
+		[Fact]
+		public void XxHash_Implementation_Constructor_Config_IsCloned()
+		{
+			var xxHashConfigMock = new Mock<IXxHashConfig>();
+			{
+				xxHashConfigMock.Setup(xhc => xhc.Clone())
+					.Returns(() => new XxHashConfig()
+					{
+						HashSizeInBits = 32,
+					});
+			}
+
+			GC.KeepAlive(
+				new XxHash_Implementation(xxHashConfigMock.Object));
+
+			xxHashConfigMock.Verify(xhc => xhc.Clone(), Times.Once);
+
+			xxHashConfigMock.VerifyGet(xhc => xhc.HashSizeInBits, Times.Never);
+			xxHashConfigMock.VerifyGet(xhc => xhc.Seed, Times.Never);
+		}
+
+		#region HashSizeInBits
+
+		[Fact]
+		public void XxHash_Implementation_Constructor_Config_HashSizeInBits_IsInvalid_Throws()
+		{
+			var invalidHashSizes = new[] { -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 31, 33, 63, 65, 127, 128, 129, 65535 };
+
+			foreach (var invalidHashSize in invalidHashSizes)
+			{
+				var xxHashConfigMock = new Mock<IXxHashConfig>();
+				{
+					xxHashConfigMock.SetupGet(pc => pc.HashSizeInBits)
+						.Returns(invalidHashSize);
+
+					xxHashConfigMock.Setup(pc => pc.Clone())
+						.Returns(() => xxHashConfigMock.Object);
+				}
+
+				Assert.Equal(
+					"config.HashSizeInBits",
+					Assert.Throws<ArgumentOutOfRangeException>(
+							() => new XxHash_Implementation(xxHashConfigMock.Object))
+						.ParamName);
+			}
+		}
+
+		[Fact]
+		public void XxHash_Implementation_Constructor_Config_HashSizeInBits_IsValid_Works()
+		{
+			var validHashSizes = new[] { 32, 64 };
+
+			foreach (var validHashSize in validHashSizes)
+			{
+				var xxHashConfigMock = new Mock<IXxHashConfig>();
+				{
+					xxHashConfigMock.SetupGet(pc => pc.HashSizeInBits)
+						.Returns(validHashSize);
+
+					xxHashConfigMock.Setup(pc => pc.Clone())
+						.Returns(() => xxHashConfigMock.Object);
+				}
+
+				GC.KeepAlive(
+					new XxHash_Implementation(xxHashConfigMock.Object));
+			}
+		}
+
+		#endregion
+
+		#endregion
+
+		#endregion
+
+		#region ComputeHash
+
+		[Fact]
+		public void XxHash_Implementation_ComputeHash_HashSizeInBits_MagicallyInvalid_Throws()
+		{
+			var xxHashConfigMock = new Mock<IXxHashConfig>();
+			{
+				var readCount = 0;
+
+				xxHashConfigMock.SetupGet(xhc => xhc.HashSizeInBits)
+					.Returns(() =>
+					{
+						readCount += 1;
+
+						if (readCount == 1)
+						{
+							return 32;
+						}
+
+						return 33;
+					});
+
+				xxHashConfigMock.Setup(xhc => xhc.Clone())
+					.Returns(() => xxHashConfigMock.Object);
+			}
+
+			var xxHash = new XxHash_Implementation(xxHashConfigMock.Object);
+
+			Assert.Throws<NotImplementedException>(
+				() => xxHash.ComputeHash(new byte[1]));
+		}
+
+		#endregion
+
+		#region ComputeHashAsync
+
+		[Fact]
+		public async Task XxHash_Implementation_ComputeHashAsync_HashSizeInBits_MagicallyInvalid_Throws()
+		{
+			var xxHashConfigMock = new Mock<IXxHashConfig>();
+			{
+				var readCount = 0;
+
+				xxHashConfigMock.SetupGet(xhc => xhc.HashSizeInBits)
+					.Returns(() =>
+					{
+						readCount += 1;
+
+						if (readCount == 1)
+						{
+							return 32;
+						}
+
+						return 33;
+					});
+
+				xxHashConfigMock.Setup(xhc => xhc.Clone())
+					.Returns(() => xxHashConfigMock.Object);
+			}
+
+			var xxHash = new XxHash_Implementation(xxHashConfigMock.Object);
+
+			using (var memoryStream = new MemoryStream(new byte[1]))
+			{
+				await Assert.ThrowsAsync<NotImplementedException>(
+					() => xxHash.ComputeHashAsync(memoryStream));
+			}
+		}
+
+		#endregion
+
+		private readonly ITestOutputHelper logger;
+		public XxHash_Implementation_Tests(ITestOutputHelper output)
+		{
+			logger = output;
+		}
+
+		public class IStreamableHashFunction_Tests_xxHash
+			: IStreamableHashFunction_TestBase<IXxHash, IXxHashConfig>
+		{
+			protected override IEnumerable<KnownValue> KnownValues { get; } =
+				new KnownValue[] {
+					new KnownValue(32, TestConstants.Empty, 0x02cc5d05),
+					new KnownValue(32, TestConstants.FooBar, 0xeda34aaf),
+					new KnownValue(32, TestConstants.LoremIpsum, 0x92ea46ac),
+					new KnownValue(32, TestConstants.LoremIpsum.Take(4), 0x0df3e9ea),
+					new KnownValue(64, TestConstants.Empty, 0xef46db3751d8e999),
+					new KnownValue(64, TestConstants.FooBar, 0xa2aa05ed9085aaf9),
+					new KnownValue(64, TestConstants.LoremIpsum, 0xaf35642971419cbe),
+					new KnownValue(64, TestConstants.LoremIpsum.Take(4), 0x103460bb4a599cab),
+				};
+
+			protected override IXxHash CreateHashFunction(int hashSize) =>
+				new XxHash_Implementation(
+					new XxHashConfig()
+					{
+						HashSizeInBits = hashSize
+					});
+		}
+
+		public class IStreamableHashFunction_Tests_xxHash_DefaultConstructor
+			: IStreamableHashFunction_TestBase<IXxHash, IXxHashConfig>
+		{
+			protected override IEnumerable<KnownValue> KnownValues { get; } =
+				new KnownValue[] {
+					new KnownValue(32, TestConstants.Empty, 0x02cc5d05),
+					new KnownValue(32, TestConstants.FooBar, 0xeda34aaf),
+					new KnownValue(32, TestConstants.LoremIpsum, 0x92ea46ac),
+				};
+
+			protected override IXxHash CreateHashFunction(int hashSize) =>
+				new XxHash_Implementation(
+					new XxHashConfig());
+		}
+
+		public class IStreamableHashFunction_Tests_xxHash_WithInitVal
+			: IStreamableHashFunction_TestBase<IXxHash, IXxHashConfig>
+		{
+			protected override IEnumerable<KnownValue> KnownValues { get; } =
+				new KnownValue[] {
+					new KnownValue(32, TestConstants.Empty, 0xff52b36b),
+					new KnownValue(32, TestConstants.FooBar, 0x294f6b05),
+					new KnownValue(32, TestConstants.LoremIpsum, 0x01f950ab),
+					new KnownValue(64, TestConstants.Empty, 0x985e09f666271418),
+					new KnownValue(64, TestConstants.FooBar, 0x947ebc3ef380d35d),
+					new KnownValue(64, TestConstants.LoremIpsum, 0xf6b6e74f681d3e5b),
+				};
+
+			protected override IXxHash CreateHashFunction(int hashSize) =>
+				new XxHash_Implementation(
+					new XxHashConfig()
+					{
+						HashSizeInBits = hashSize,
+						Seed = 0x78fef705b7c769faU
+					});
+		}
+	}
+}
