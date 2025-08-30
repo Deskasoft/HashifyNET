3bd6ba9778427553300ce0020f82267aa3110aa9
Xen <lordofxen@deskasoft.com>
2025-08-31 01:20:11 +0300

    Push the new xxHash implementation
    Signed-off-by: Xen <lordofxen@deskasoft.com>

---
+++ HashifyNet/Algorithms/XxHash/XxHash3_Implementation.cs
@@ -0,0 +1,104 @@
+﻿// *
+// *****************************************************************************
+// *
+// * Copyright (c) 2025 Deskasoft International
+// *
+// * Permission is hereby granted, free of charge, to any person obtaining a copy
+// * of this software and associated documentation files (the ""Software""), to deal
+// * in the Software without restriction, including without limitation the rights
+// * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// * copies of the Software, and to permit persons to whom the Software is
+// * furnished to do so, subject to the following conditions:
+// *
+// * The above copyright notice and this permission notice shall be included in all
+// * copies or substantial portions of the Software.
+// *
+// * THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// * SOFTWARE.
+// *
+// *
+// * Please refer to LICENSE file.
+// *
+// ******************************************************************************
+// *
+
+using HashifyNet.Core;
+using HashifyNet.Core.Utilities;
+using System;
+using System.Threading;
+
+namespace HashifyNet.Algorithms.XxHash3
+{
+	internal class XxHash3_Implementation
+		: StreamableHashFunctionBase<IXxHash3Config>,
+			IXxHash3
+	{
+		public override IXxHash3Config Config => _config.Clone();
+
+		private readonly IXxHash3Config _config;
+		public XxHash3_Implementation(IXxHash3Config config)
+		{
+			if (config == null)
+			{
+				throw new ArgumentNullException(nameof(config));
+			}
+
+			_config = config.Clone();
+
+			if (_config.HashSizeInBits != 64 && _config.HashSizeInBits != 128)
+			{
+				throw new ArgumentOutOfRangeException($"{nameof(config)}.{nameof(config.HashSizeInBits)}", _config.HashSizeInBits, $"{nameof(config)}.{nameof(config.HashSizeInBits)} must be fixed at 64 or 128 bits.");
+			}
+		}
+
+		public override IBlockTransformer CreateBlockTransformer()
+		{
+			return new BlockTransformer3(_config.HashSizeInBits, _config.Seed);
+		}
+
+		private class BlockTransformer3 : BlockTransformerBase<BlockTransformer3>
+		{
+			public BlockTransformer3() : base(inputBlockSize: 64) { }
+
+			private System.IO.Hashing.NonCryptographicHashAlgorithm _hash;
+			public BlockTransformer3(int hashSize, ulong seed) : this()
+			{
+				switch (hashSize)
+				{
+					case 64:
+						_hash = new System.IO.Hashing.XxHash3((long)seed);
+						break;
+					case 128:
+						_hash = new System.IO.Hashing.XxHash128((long)seed);
+						break;
+					default:
+						throw new NotImplementedException();
+				}
+			}
+
+			protected override void CopyStateTo(BlockTransformer3 other)
+			{
+				other._hash = _hash;
+			}
+
+			protected override void TransformByteGroupsInternal(ArraySegment<byte> data)
+			{
+				_hash.Append(data.AsSpan());
+			}
+
+			protected override IHashValue FinalizeHashValueInternal(CancellationToken cancellationToken)
+			{
+				Span<byte> last = FinalizeInputBuffer == null ? Span<byte>.Empty : FinalizeInputBuffer.AsSpan();
+				_hash.Append(last);
+
+				byte[] output = _hash.GetCurrentHash();
+				return new HashValue(output, _hash.HashLengthInBytes * 8);
+			}
+		}
+	}
+}
---
+++ HashifyNet/Algorithms/XxHash/XxHash_Implementation.cs
@@ -0,0 +1,448 @@
+// *
+// *****************************************************************************
+// *
+// * Copyright (c) 2025 Deskasoft International
+// *
+// * Permission is hereby granted, free of charge, to any person obtaining a copy
+// * of this software and associated documentation files (the "Software"), to deal
+// * in the Software without restriction, including without limitation the rights
+// * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// * copies of the Software, and to permit persons to whom the Software is
+// * furnished to do so, subject to the following conditions:
+// *
+// * The above copyright notice and this permission notice shall be included in all
+// * copies or substantial portions of the Software.
+// *
+// * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// * SOFTWARE.
+// *
+// *
+// * Please refer to LICENSE file.
+// *
+// ******************************************************************************
+// *
+
+using HashifyNet.Core;
+using HashifyNet.Core.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace HashifyNet.Algorithms.XxHash
+{
+	/// <summary>
+	/// Provides an implementation of the xxHash algorithm, a fast, non-cryptographic hash function.
+	/// </summary>
+	/// <remarks>This class supports both 32-bit and 64-bit variants of the xxHash algorithm, determined by the <see
+	/// cref="IHashConfigBase.HashSizeInBits"/> property of the configuration provided during construction. The xxHash
+	/// algorithm is optimized for speed and is suitable for scenarios such as checksums, data integrity verification, and
+	/// hash-based lookups. It is not suitable for cryptographic purposes.</remarks>
+	[HashAlgorithmImplementation(typeof(IXxHash), typeof(XxHashConfig))]
+	internal partial class XxHash_Implementation
+		: StreamableHashFunctionBase<IXxHashConfig>,
+			IXxHash
+	{
+		public override IXxHashConfig Config => _config.Clone();
+
+		private readonly IXxHashConfig _config;
+		private static readonly IEnumerable<int> _validHashSizes = new HashSet<int>() { 32, 64 };
+		public XxHash_Implementation(IXxHashConfig config)
+		{
+			if (config == null)
+			{
+				throw new ArgumentNullException(nameof(config));
+			}
+
+			_config = config.Clone();
+
+			if (!_validHashSizes.Contains(_config.HashSizeInBits))
+			{
+				throw new ArgumentOutOfRangeException($"{nameof(config)}.{nameof(config.HashSizeInBits)}", _config.HashSizeInBits, $"{nameof(config)}.{nameof(config.HashSizeInBits)} must be contained within xxHash.ValidHashSizes");
+			}
+		}
+
+		public override IBlockTransformer CreateBlockTransformer()
+		{
+			switch (_config.HashSizeInBits)
+			{
+				case 32:
+					return new BlockTransformer32((uint)_config.Seed);
+
+				case 64:
+					return new BlockTransformer64(_config.Seed);
+
+				default:
+					throw new NotImplementedException();
+			}
+		}
+
+		private class BlockTransformer32
+			: BlockTransformerBase<BlockTransformer32>
+		{
+			private static readonly uint[] _primes32 =
+				new[] {
+					2654435761U,
+					2246822519U,
+					3266489917U,
+					 668265263U,
+					 374761393U
+				};
+
+			private uint _seed;
+
+			private uint _a;
+			private uint _b;
+			private uint _c;
+			private uint _d;
+
+			private ulong _bytesProcessed = 0;
+
+			public BlockTransformer32()
+				: base(inputBlockSize: 16)
+			{
+
+			}
+
+			public BlockTransformer32(uint seed)
+				: this()
+			{
+				_seed = seed;
+
+				_a = _seed + _primes32[0] + _primes32[1];
+				_b = _seed + _primes32[1];
+				_c = _seed;
+				_d = _seed - _primes32[0];
+			}
+
+			protected override void CopyStateTo(BlockTransformer32 other)
+			{
+				base.CopyStateTo(other);
+
+				other._seed = _seed;
+
+				other._a = _a;
+				other._b = _b;
+				other._c = _c;
+				other._d = _d;
+
+				other._bytesProcessed = _bytesProcessed;
+			}
+
+			protected override void TransformByteGroupsInternal(ArraySegment<byte> data)
+			{
+				var dataArray = data.Array;
+				var dataOffset = data.Offset;
+				var dataCount = data.Count;
+
+				var endOffset = dataOffset + dataCount;
+
+				var tempA = _a;
+				var tempB = _b;
+				var tempC = _c;
+				var tempD = _d;
+
+				var tempPrime0 = _primes32[0];
+				var tempPrime1 = _primes32[1];
+
+				for (var currentIndex = dataOffset; currentIndex < endOffset; currentIndex += 16)
+				{
+					tempA += BitConverter.ToUInt32(dataArray, currentIndex) * tempPrime1;
+					tempA = RotateLeft(tempA, 13);
+					tempA *= tempPrime0;
+
+					tempB += BitConverter.ToUInt32(dataArray, currentIndex + 4) * tempPrime1;
+					tempB = RotateLeft(tempB, 13);
+					tempB *= tempPrime0;
+
+					tempC += BitConverter.ToUInt32(dataArray, currentIndex + 8) * tempPrime1;
+					tempC = RotateLeft(tempC, 13);
+					tempC *= tempPrime0;
+
+					tempD += BitConverter.ToUInt32(dataArray, currentIndex + 12) * tempPrime1;
+					tempD = RotateLeft(tempD, 13);
+					tempD *= tempPrime0;
+				}
+
+				_a = tempA;
+				_b = tempB;
+				_c = tempC;
+				_d = tempD;
+
+				_bytesProcessed += (ulong)dataCount;
+			}
+
+			protected override IHashValue FinalizeHashValueInternal(CancellationToken cancellationToken)
+			{
+				uint hashValue;
+				{
+					if (_bytesProcessed > 0)
+					{
+						hashValue = RotateLeft(_a, 1) + RotateLeft(_b, 7) + RotateLeft(_c, 12) + RotateLeft(_d, 18);
+					}
+					else
+					{
+						hashValue = _seed + _primes32[4];
+					}
+				}
+
+				var remainder = FinalizeInputBuffer;
+				var remainderLength = (remainder?.Length).GetValueOrDefault();
+
+				hashValue += (uint)(_bytesProcessed + (ulong)remainderLength);
+
+				if (remainderLength > 0)
+				{
+					// In 4-byte chunks, process all process all full chunks
+					{
+						var endOffset = remainderLength - (remainderLength % 4);
+
+						for (var currentOffset = 0; currentOffset < endOffset; currentOffset += 4)
+						{
+							hashValue += BitConverter.ToUInt32(remainder, currentOffset) * _primes32[2];
+							hashValue = RotateLeft(hashValue, 17) * _primes32[3];
+						}
+					}
+
+					// Process last 4 bytes in 1-byte chunks (only runs if data.Length % 4 != 0)
+					{
+						var startOffset = remainderLength - (remainderLength % 4);
+						var endOffset = remainderLength;
+
+						for (var currentOffset = startOffset; currentOffset < endOffset; currentOffset += 1)
+						{
+							hashValue += remainder[currentOffset] * _primes32[4];
+							hashValue = RotateLeft(hashValue, 11) * _primes32[0];
+						}
+					}
+				}
+
+				hashValue ^= hashValue >> 15;
+				hashValue *= _primes32[1];
+				hashValue ^= hashValue >> 13;
+				hashValue *= _primes32[2];
+				hashValue ^= hashValue >> 16;
+
+				return new HashValue(
+					BitConverter.GetBytes(hashValue),
+					32);
+			}
+
+			private static uint RotateLeft(uint operand, int shiftCount)
+			{
+				shiftCount &= 0x1f;
+
+				return
+					(operand << shiftCount) |
+					(operand >> (32 - shiftCount));
+			}
+		}
+
+		private class BlockTransformer64
+			: BlockTransformerBase<BlockTransformer64>
+		{
+			private static readonly IReadOnlyList<ulong> _primes64 =
+				new[] {
+					11400714785074694791UL,
+					14029467366897019727UL,
+					 1609587929392839161UL,
+					 9650029242287828579UL,
+					 2870177450012600261UL
+				};
+
+			private ulong _seed;
+
+			private ulong _a;
+			private ulong _b;
+			private ulong _c;
+			private ulong _d;
+
+			private ulong _bytesProcessed = 0;
+
+			public BlockTransformer64()
+				: base(inputBlockSize: 32)
+			{
+			}
+
+			public BlockTransformer64(ulong seed)
+				: this()
+			{
+				_seed = seed;
+
+				_a = _seed + _primes64[0] + _primes64[1];
+				_b = _seed + _primes64[1];
+				_c = _seed;
+				_d = _seed - _primes64[0];
+			}
+
+			protected override void CopyStateTo(BlockTransformer64 other)
+			{
+				base.CopyStateTo(other);
+
+				other._seed = _seed;
+
+				other._a = _a;
+				other._b = _b;
+				other._c = _c;
+				other._d = _d;
+
+				other._bytesProcessed = _bytesProcessed;
+			}
+
+			protected override void TransformByteGroupsInternal(ArraySegment<byte> data)
+			{
+				var dataArray = data.Array;
+				var dataOffset = data.Offset;
+				var dataCount = data.Count;
+
+				var endOffset = dataOffset + dataCount;
+
+				var tempA = _a;
+				var tempB = _b;
+				var tempC = _c;
+				var tempD = _d;
+
+				var tempPrime0 = _primes64[0];
+				var tempPrime1 = _primes64[1];
+
+				for (var currentIndex = dataOffset; currentIndex < endOffset; currentIndex += 32)
+				{
+					tempA += BitConverter.ToUInt64(dataArray, currentIndex) * tempPrime1;
+					tempA = RotateLeft(tempA, 31);
+					tempA *= tempPrime0;
+
+					tempB += BitConverter.ToUInt64(dataArray, currentIndex + 8) * tempPrime1;
+					tempB = RotateLeft(tempB, 31);
+					tempB *= tempPrime0;
+
+					tempC += BitConverter.ToUInt64(dataArray, currentIndex + 16) * tempPrime1;
+					tempC = RotateLeft(tempC, 31);
+					tempC *= tempPrime0;
+
+					tempD += BitConverter.ToUInt64(dataArray, currentIndex + 24) * tempPrime1;
+					tempD = RotateLeft(tempD, 31);
+					tempD *= tempPrime0;
+				}
+
+				_a = tempA;
+				_b = tempB;
+				_c = tempC;
+				_d = tempD;
+
+				_bytesProcessed += (ulong)dataCount;
+			}
+
+			protected override IHashValue FinalizeHashValueInternal(CancellationToken cancellationToken)
+			{
+				ulong hashValue;
+				{
+					if (_bytesProcessed > 0)
+					{
+						var tempA = _a;
+						var tempB = _b;
+						var tempC = _c;
+						var tempD = _d;
+
+						hashValue = RotateLeft(_a, 1) + RotateLeft(_b, 7) + RotateLeft(_c, 12) + RotateLeft(_d, 18);
+
+						// A
+						tempA *= _primes64[1];
+						tempA = RotateLeft(tempA, 31);
+						tempA *= _primes64[0];
+
+						hashValue ^= tempA;
+						hashValue = (hashValue * _primes64[0]) + _primes64[3];
+
+						// B
+						tempB *= _primes64[1];
+						tempB = RotateLeft(tempB, 31);
+						tempB *= _primes64[0];
+
+						hashValue ^= tempB;
+						hashValue = (hashValue * _primes64[0]) + _primes64[3];
+
+						// C
+						tempC *= _primes64[1];
+						tempC = RotateLeft(tempC, 31);
+						tempC *= _primes64[0];
+
+						hashValue ^= tempC;
+						hashValue = (hashValue * _primes64[0]) + _primes64[3];
+
+						// D
+						tempD *= _primes64[1];
+						tempD = RotateLeft(tempD, 31);
+						tempD *= _primes64[0];
+
+						hashValue ^= tempD;
+						hashValue = (hashValue * _primes64[0]) + _primes64[3];
+					}
+					else
+					{
+						hashValue = _seed + _primes64[4];
+					}
+				}
+
+				var remainder = FinalizeInputBuffer;
+				var remainderLength = (remainder?.Length).GetValueOrDefault();
+
+				hashValue += _bytesProcessed + (ulong)remainderLength;
+
+				if (remainderLength > 0)
+				{
+					// In 8-byte chunks, process all full chunks
+					for (int x = 0; x < remainder.Length / 8; ++x)
+					{
+						hashValue ^= RotateLeft(BitConverter.ToUInt64(remainder, x * 8) * _primes64[1], 31) * _primes64[0];
+						hashValue = (RotateLeft(hashValue, 27) * _primes64[0]) + _primes64[3];
+					}
+
+					// Process a 4-byte chunk if it exists
+					if ((remainderLength % 8) >= 4)
+					{
+						var startOffset = remainderLength - (remainderLength % 8);
+
+						hashValue ^= BitConverter.ToUInt32(remainder, startOffset) * _primes64[0];
+						hashValue = (RotateLeft(hashValue, 23) * _primes64[1]) + _primes64[2];
+					}
+
+					// Process last 4 bytes in 1-byte chunks (only runs if data.Length % 4 != 0)
+					{
+						var startOffset = remainderLength - (remainderLength % 4);
+						var endOffset = remainderLength;
+
+						for (var currentOffset = startOffset; currentOffset < endOffset; currentOffset += 1)
+						{
+							hashValue ^= remainder[currentOffset] * _primes64[4];
+							hashValue = RotateLeft(hashValue, 11) * _primes64[0];
+						}
+					}
+				}
+
+				hashValue ^= hashValue >> 33;
+				hashValue *= _primes64[1];
+				hashValue ^= hashValue >> 29;
+				hashValue *= _primes64[2];
+				hashValue ^= hashValue >> 32;
+
+				return new HashValue(
+					BitConverter.GetBytes(hashValue),
+					64);
+			}
+
+			private static ulong RotateLeft(ulong operand, int shiftCount)
+			{
+				shiftCount &= 0x3f;
+
+				return
+					(operand << shiftCount) |
+					(operand >> (64 - shiftCount));
+			}
+		}
+	}
+}
