1e36f1ba65dfcf8e9da698b9c55bd04090406f62
Xen <lordofxen@deskasoft.com>
2025-09-09 20:35:46 -0700

    Pad fields
    Signed-off-by: Xen <lordofxen@deskasoft.com>

--- HashifyNet/Core/Factory/HashFactory.cs
+++ HashifyNet/Core/Factory/HashFactory.cs
@@ -63,6 +63,7 @@ namespace HashifyNet
 		private static readonly Hashtable _implementations;
 		private static readonly Hashtable _concreteConfigTypes;
 		private static readonly Hashtable _configProfiles;
+  		
 		/// <summary>
 		/// Initializes the static state of the <see cref="HashFactory"/> class by discovering and registering available hash
 		/// algorithm implementations.
@@ -269,3 +270,4 @@ namespace HashifyNet
 		}
 	}
 }
+
