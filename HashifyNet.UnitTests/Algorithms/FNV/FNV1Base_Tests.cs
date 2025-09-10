7173a857d1c07ee5f8632a12051454d6d17b395b
Xen <lordofxen@deskasoft.com>
2025-09-10 15:54:54 -0700

    Refactor FNV1Base_Tests
    Signed-off-by: Xen <lordofxen@deskasoft.com>

--- HashifyNet.UnitTests/Algorithms/FNV/FNV1Base_Tests.cs
+++ HashifyNet.UnitTests/Algorithms/FNV/FNV1Base_Tests.cs
@@ -41,21 +41,20 @@ namespace HashifyNet.UnitTests.Algorithms.FNV
 			{
 			}
 
-			public static UInt32[] _ExtendedMultiply(IReadOnlyList<UInt32> operand1, IReadOnlyList<UInt32> operand2, int hashSizeInBytes) =>
+			public static uint[] ExtendedMultiplyTest(IReadOnlyList<uint> operand1, IReadOnlyList<uint> operand2, int hashSizeInBytes) =>
 				ExtendedMultiply(operand1, operand2, hashSizeInBytes);
-
 		}
 
 		[Fact]
 		public void FNV1Base_ExtendedMultiply_WorksConversly()
 		{
-			var x = new UInt32[] { 65536, 1024 };
-			var y = new UInt32[] { 524288, 65536, 1024, 8 };
+			var x = new uint[] { 65536, 1024 };
+			var y = new uint[] { 524288, 65536, 1024, 8 };
 
-			var expectedValue = new UInt32[] { 0, 536870920, 134217729, 1572864 };
+			var expectedValue = new uint[] { 0, 536870920, 134217729, 1572864 };
 
-			Assert.Equal(expectedValue, FNV1Impl._ExtendedMultiply(x, y, 16));
-			Assert.Equal(expectedValue, FNV1Impl._ExtendedMultiply(y, x, 16));
+			Assert.Equal(expectedValue, FNV1Impl.ExtendedMultiplyTest(x, y, 16));
+			Assert.Equal(expectedValue, FNV1Impl.ExtendedMultiplyTest(y, x, 16));
 		}
 	}
 }
