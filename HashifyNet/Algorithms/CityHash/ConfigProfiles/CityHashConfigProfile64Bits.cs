e67b71aaee77cb6ce8bb38a3fd6879174bb4fff1
Xen <lordofxen@deskasoft.com>
2025-09-15 10:00:59 +0300

    Add config profiles for City Hash
    Signed-off-by: Xen <lordofxen@deskasoft.com>

---
+++ HashifyNet/Algorithms/CityHash/ConfigProfiles/CityHashConfigProfile64Bits.cs
@@ -0,0 +1,49 @@
+﻿// *
+// *****************************************************************************
+// *
+// * Copyright (c) 2025 Deskasoft International
+// *
+// * Permission is hereby granted, free of charge, to any person obtaining a copy
+// * of this software and associated documentation files (the ""Software""), to deal
+// * in the Software without restriction, including without limitation the rights
+// * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// * copies of the Software, and to permit persons to whom the Software is
+// * furnished to do so, subject to the following conditions:
+// *
+// * The above copyright notice and this permission notice shall be included in all
+// * copies or substantial portions of the Software.
+// *
+// * THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// * SOFTWARE.
+// *
+// *
+// * Please refer to LICENSE file.
+// *
+// ******************************************************************************
+// *
+
+using HashifyNet.Core;
+
+namespace HashifyNet.Algorithms.CityHash
+{
+	/// <summary>
+	/// Default config profile for 64 bit computation using City Hash.
+	/// </summary>
+	[DefineHashConfigProfile("64Bits", "Default config profile for 64 bit computation using City Hash.")]
+	public sealed class CityHashConfigProfile64Bits : CityHashConfig
+	{
+		/// <summary>
+		/// Creates a new instance of <see cref="CityHashConfigProfile64Bits"/>.
+		/// <para>Sets <see cref="ICityHashConfig.HashSizeInBits"/> to <c>64</c>.</para>
+		/// </summary>
+		public CityHashConfigProfile64Bits()
+		{
+			HashSizeInBits = 64;
+		}
+	}
+}
