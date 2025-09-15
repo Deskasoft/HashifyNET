bee7defcae8ba8494d0e6b58c31a3bae884b13b2
Xen <lordofxen@deskasoft.com>
2025-09-15 08:58:47 +0300

    Add config profiles for RapidHash
    Signed-off-by: Xen <lordofxen@deskasoft.com>

---
+++ HashifyNet/Algorithms/RapidHash/ConfigProfiles/RapidHashConfigProfileNano.cs
@@ -0,0 +1,51 @@
+﻿// *
+// *****************************************************************************
+// *
+// * Copyright (c) 2025 Deskasoft International
+// *
+// * Permission is hereby granted, free of charge, to any person obtaining a copy
+// * of this software and associated documentation files (the ""Software""), to deal
+// * in the Software without restriction, including without limitation the rights
+// * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// * copies of the Software, and to permit persons to whom the Software is
+// * furnished to do so, subject to the following conditions:
+// *
+// * The above copyright notice and this permission notice shall be included in all
+// * copies or substantial portions of the Software.
+// *
+// * THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// * SOFTWARE.
+// *
+// *
+// * Please refer to LICENSE file.
+// *
+// ******************************************************************************
+// *
+
+using HashifyNet.Core;
+
+namespace HashifyNet.Algorithms.RapidHash
+{
+	/// <summary>
+	/// Represents a predefined configuration profile for rapid hash computation in Nano mode.
+	/// </summary>
+	/// <remarks>This profile is optimized for scenarios requiring high-speed hash computation with minimal resource
+	/// usage. The configuration mode is set to <see cref="RapidHashMode.Nano"/> by default.</remarks>
+	[DefineHashConfigProfile("Nano", "Defaults to Nano mode for Rapid Hash Computation.")]
+	public sealed class RapidHashConfigProfileNano : RapidHashConfig
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="RapidHashConfigProfileNano"/> class with the configuration mode set
+		/// to <see cref="RapidHashMode.Nano"/>.
+		/// </summary>
+		public RapidHashConfigProfileNano()
+		{
+			Mode = RapidHashMode.Nano;
+		}
+	}
+}
