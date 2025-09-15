44f5fb503f98a35e7df097c3bdd902b4442abd12
Xen <lordofxen@deskasoft.com>
2025-09-15 09:36:07 +0300

    Add config profiles for Spooky Hash
    Signed-off-by: Xen <lordofxen@deskasoft.com>

---
+++ HashifyNet/Algorithms/SpookyHash/ConfigProfiles/SpookyHashConfigProfile64Bits.cs
@@ -0,0 +1,48 @@
+﻿// *
+// *****************************************************************************
+// *
+// * Copyright (c) 2025 Deskasoft International
+// *
+// * Permission is hereby granted, free of charge, to any person obtaining a copy
+// * of this software and associated documentation files (the ""Software""), to deal
+// * in the Software without restriction, including without limitation the rights
+// * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// * copies of the Software, and to permit persons to whom the Software is
+// * furnished to do so, subject to the following conditions:
+// *
+// * The above copyright notice and this permission notice shall be included in all
+// * copies or substantial portions of the Software.
+// *
+// * THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// * SOFTWARE.
+// *
+// *
+// * Please refer to LICENSE file.
+// *
+// ******************************************************************************
+// *
+
+using HashifyNet.Core;
+
+namespace HashifyNet.Algorithms.SpookyHash
+{
+	/// <summary>
+	/// Represents a predefined configuration profile for SpookyHash that generates 64-bit hash values.
+	/// </summary>
+	[DefineHashConfigProfile("64Bits", "Provides a default 64 bit config for Spooky Hash.")]
+	public sealed class SpookyHashConfigProfile64Bits : SpookyHashConfig
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="SpookyHashConfigProfile64Bits"/> class.
+		/// </summary>
+		public SpookyHashConfigProfile64Bits()
+		{
+			HashSizeInBits = 64;
+		}
+	}
+}
